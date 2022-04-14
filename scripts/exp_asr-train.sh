c92f20997bcfb2200251fd4531dd2fd45748fb52
Nay San <nay.san@stanford.edu>
2022-04-14 15:01:54 -0700

    Add ASR training script

---
+++ scripts/exp_asr-train.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/bash
+
+# Data subset experiments 
+declare -a subsets=("train-100" "train-80" "train-60" "train-40" "train-20")
+
+for i in "${subsets[@]}"
+do
+   python scripts/train_asr-by-w2v2-ft.py \
+   facebook/wav2vec2-large-robust-ft-swbd-300h \
+   "data/exps/asr/checkpoints/$i" \
+   "data/exps/asr/datasets/$i.tsv" \
+    data/exps/asr/datasets/test.tsv
+done
+
+# Cross-validation experiments without language model
+for j in {1..10}
+do
+   python scripts/train_asr-by-w2v2-ft.py \
+   facebook/wav2vec2-large-robust-ft-swbd-300h \
+   "data/exps/asr/checkpoints/bootstrap/no-lm/b-$j" \
+   "data/exps/asr/datasets/bootstrap-$j-train60.tsv" \
+   "data/exps/asr/datasets/bootstrap-$j-test20.tsv"
+done
+
+# Cross-validation experiments with a bigram language model
+for k in {1..10}
+do
+   python scripts/train_asr-by-w2v2-ft.py \
+   facebook/wav2vec2-large-robust-ft-swbd-300h \
+   "data/exps/asr/checkpoints/bootstrap/lm/b-$k" \
+   "data/exps/asr/datasets/bootstrap-$k-train60.tsv" \
+   "data/exps/asr/datasets/bootstrap-$k-test20.tsv" \
+   --lm_arpa data/exps/asr/2gram_correct.arpa
+done
